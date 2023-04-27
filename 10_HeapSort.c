f0b509ac422fcf455197d3e9cb59ac1921f34c41
Tushar Singh <99042369+CodeTusharSingh@users.noreply.github.com>
2023-04-27 21:34:20 +0530

    Update 10_HeapSort.c

--- 10_HeapSort.c
+++ 10_HeapSort.c
@@ -1,44 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct tree
-{
-    int value;
-    struct tree *left;
-    struct tree *right;
-};
-
-struct tree *createTree(int value)
-{
-    struct tree *node = malloc(sizeof(struct tree));
-    node->value = value;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-}
-
-struct tree *buildTree(int arr[], int start, int end)
-{
-    if (start > end)
-    {
-        return NULL;
-    }
-    int mid = (start + end) / 2;
-    struct tree *root = createTree(arr[mid]);
-    root->left = buildTree(arr, start, mid - 1);
-    root->right = buildTree(arr, mid + 1, end);
-    return root;
-}
-
-void traverse(struct tree *root)
-{
-    if (root == NULL)
-        return;
-    printf("%d ", root->value);
-    traverse(root->left);
-    traverse(root->right);
-}
-
 int Parent(int i)
 {
     return (i - 1) / 2;
@@ -109,10 +71,6 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
-    struct tree *root = buildTree(arr, 0, size - 1);
-    printf("Tree : ");
-    traverse(root);
-    printf("\n");
     heapSort(arr, size);
     for (int i = 0; i < size; i++)
     {
