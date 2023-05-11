71734494594a2984db742f69284ac494334bcffe
Tushar Singh <99042369+CodeTusharSingh@users.noreply.github.com>
2023-05-11 23:06:41 +0530

    Add files via upload

---
+++ 1_Factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    printf("%d \n", factorial(5));
+    return 0;
+}
